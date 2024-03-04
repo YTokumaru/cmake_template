b86672b176ef95cf432f3900acc999a80e08ffdd
YTokumaru <ytokumaru16@gmail.com>
2024-03-04 16:25:31 +0900

    circumvent a bug with clang-cl and --coverage

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -4,6 +4,17 @@ function(myproject_enable_coverage project_name)
     return()
   endif()
   if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+    if(WIN32)
+      # There is a bug regarding --coverage on Visual Studio llvm toolchain manual linking is required to circumvent
+      # https://bugs.llvm.org/show_bug.cgi?id=40877
+      execute_process(
+        COMMAND cmd /c "C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe" -latest -products * -find  **/lib/clang/17/lib/windows/clang_rt.profile-x86_64.lib
+        OUTPUT_VARIABLE LIB_INSTALL_DIR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+      target_link_libraries(${project_name} INTERFACE "${LIB_INSTALL_DIR}")
+      # target_link_libraries(${project_name} INTERFACE "C:\\Program Files (x86)\\Microsoft Visual Studio\\2022\\BuildTools\\VC\\Tools\\Llvm\\x64\\lib\\clang\\17\\lib\\windows\\clang_rt.profile-x86_64.lib")
+    endif()
     target_compile_options(${project_name} INTERFACE --coverage)
     target_link_libraries(${project_name} INTERFACE --coverage)
   endif()
