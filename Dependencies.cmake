9805fefcb701b9d21dfeb4adec446313a6ed41b8
YTokumaru <ytokumaru16@gmail.com>
2024-03-11 13:35:27 +0900

    add boost with mpi to dependency

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -61,4 +61,18 @@ function(myproject_setup_dependencies)
         "FTXUI_BUILD_DOCS OFF"
         "FTXUI_BUILD_EXAMPLES OFF")
   endif()
+
+  if(NOT TARGET boost_mpi)
+    # boost is a huge project and directly downloading the 'alternate release'
+    # from github is much faster than recursively cloning the repo.
+    CPMAddPackage(
+      NAME Boost
+      VERSION 1.84.0
+      URL https://github.com/boostorg/boost/releases/download/boost-1.84.0/boost-1.84.0.tar.xz
+      URL_HASH SHA256=2e64e5d79a738d0fa6fb546c6e5c2bd28f88d268a2a080546f74e5ff98f29d0e
+      OPTIONS 
+        "BOOST_ENABLE_CMAKE ON"
+        "BOOST_ENABLE_MPI ON"
+    )
+  endif()
 endfunction()
